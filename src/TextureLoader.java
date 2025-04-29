095dae40a96ed6d8feace463f01e147ba0f6d65b
vladimirrainov <raynov.vladimir@gmail.com>
2025-04-29 19:37:41 +0300

    Texture Loader now has an exception handler

--- src/TextureLoader.java
+++ src/TextureLoader.java
@@ -10,7 +10,19 @@ public class TextureLoader {
         try {
             wallTexture = ImageIO.read(new File("Rays_2d_SoftDev-main/src/textures/brick.png"));
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Failed to load wall texture. Using default texture.");
+            wallTexture = createDefaultTexture(); // Use default texture
         }
+    }
+
+    private static BufferedImage createDefaultTexture() {
+        BufferedImage defaultTexture = new BufferedImage(64, 64, BufferedImage.TYPE_INT_RGB);
+        for (int y = 0; y < 64; y++) {
+            for (int x = 0; x < 64; x++) {
+                int color = (x / 8 % 2 == y / 8 % 2) ? 0xFFFFFF : 0xAAAAAA; // Checkerboard pattern
+                defaultTexture.setRGB(x, y, color);
+            }
+        }
+        return defaultTexture;
     }
 }
